251c5354c73a4c674ca5cb30d192ad6c29b49a5c
Akshay <n.akshay19@gmail.com>
2019-02-09 19:15:48 -0600

    Use StringUtils and added dependencies

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -2,12 +2,18 @@ package clinic.programming.training;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     
     public Application() {
         System.out.println ("Inside Application");
     }
+	
+	public int countWords(String sentence){
+		String [] separateWords = StringUtils.split(sentence,' ');
+		return (separateWords == null) ? 0 : separateWords.length;
+	}
 	
 	public void greet(){
 		List<String> greetings = new ArrayList<>();
@@ -21,8 +27,9 @@ public class Application {
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
+    	System.out.println("Starting Application");
 		Application app = new Application();
 		app.greet();
+		System.out.println("Word Count : " + app.countWords("How are you doing today?"));
     }
 }
